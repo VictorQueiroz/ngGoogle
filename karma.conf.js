4ad23153cd0f8938ff33cabf057a05cb401771bf
Victor Queiroz <victorcqueirozg@gmail.com>
2014-10-06 19:05:28 -0300

    chore(dist builds fixed): release 1.0.2

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       'dist/angular-google.tpl.min.js',
       'src/**/*.js',
+      'test/**/*.Spec.js',
     ],
 
 
